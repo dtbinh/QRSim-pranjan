7718e5da386dcbeace640515b965d397db6cf242
Pritesh Ranjan <pritesh.ranjan.trivedi@gmail.com>
2018-06-28 13:44:23 -0400

    Write plots to png files

--- ababujo/probability.m
+++ ababujo/probability.m
@@ -1,4 +1,5 @@
 function x = probability()
+    fontsize = 40;
     T = 17;
     Dth = -87;        % dBm, Threshold for a successful message receipt.
     N = 500;
@@ -9,24 +10,27 @@ function x = probability()
     for i=1:N
         recv(i) = helper(T, i, F, mmean, sigma);
     end
-    figure();
-    subplot(2,1,1)
+    FigH = figure('Position', get(0, 'Screensize'));   
+    %subplot(2,1,1);
     plot(recv);
     hold on;
     for i=1:N
         recv(i) = helper(T, i, F, mmean, 0);
     end
-    text(250, -40, ['T= ', num2str(T) , ' dBm, F= ',  num2str(F),  ' MHz,  sigma= ',  num2str(sigma)], 'FontSize', 20);
+    text(100, -20, ['T= ', num2str(T) , ' dBm, F= ',  num2str(F),  ' MHz,  $$\sigma$$= ',  num2str(sigma)], 'FontSize', fontsize, 'Interpreter', 'latex');
     plot(recv)
     grid on;
     hold off;
-    xlabel("Distance (m)", 'FontSize', 20);
-    ylabel("dBm", 'FontSize', 20);
-    title("Received signal strength vs Distance", 'FontSize', 20);
+    xlabel("Distance (m)", 'FontSize', fontsize, 'Interpreter', 'latex');
+    ylabel("Signal Strength (dBm)", 'FontSize', fontsize, 'Interpreter', 'latex');
+    set(gca, 'FontSize', fontsize)
+    title("Received signal strength vs Distance", 'FontSize', fontsize, 'Interpreter', 'latex');
     ylim([-110 0]);
-    %title([' T= ', num2str(T), ' dBm, F= ', num2str(F), ' m, mean= ', num2str(mmean), ', sigma= ', num2str(sigma)]);
+    Fr = getframe(FigH);
+    imwrite(Fr.cdata, 'signal_strength.png', 'png')
     
-    subplot(2,1,2)
+    FigH = figure('Position', get(0, 'Screensize'));   
+    %subplot(2,1,2)
     %dsts = [10, sqrt(2) * 10, 20, sqrt(2) * 20, 30, 30 * sqrt(2)];
     dsts = 1:N;
     arrrs = zeros(length(dsts));
@@ -45,10 +49,14 @@ function x = probability()
     plot(dsts, arrrs);
     grid on;
     hold on;
-    text(300, 600, ['T= ', num2str(T) , ' dBm, F= ',  num2str(F),  ' MHz,  sigma= ',  num2str(sigma)], 'FontSize', 20);
-    ylabel("Lost packets count", 'FontSize', 20);
-    xlabel("Distance (m)", 'FontSize', 20);
-    title("Packet loss count per 1000 packets", 'FontSize', 20);
+    text(200, 100, ['T= ', num2str(T) , ' dBm, F= ',  num2str(F),  ' MHz,  $$\sigma$$= ',  num2str(sigma)], 'FontSize', fontsize, 'Interpreter', 'latex');
+    ylabel("Lost packets count", 'FontSize', fontsize, 'Interpreter', 'latex');
+    xlabel("Distance (m)", 'FontSize', fontsize, 'Interpreter', 'latex');
+    set(gca, 'FontSize', fontsize)
+    title_1 = sprintf("Packet loss vs Distance");
+    title(title_1, 'FontSize', fontsize, 'Interpreter', 'latex');  
+    Fr    = getframe(FigH);
+    imwrite(Fr.cdata, 'packet_loss.png', 'png')
 end
 
 function RecPower = helper(T, D, F, mmean, sigma)
